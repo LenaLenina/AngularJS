b3f5467d1856d899451ba051148c8b21d5a929a2
LenaLenina <lenalenina5677@gmail.com>
2019-02-05 22:42:26 +0300

    Добавьте файлы проекта.

---
+++ 004_Controllers/Training/AngularJSControllers/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(a);
+
+                //a = a + 1;
+                //Console.WriteLine(a++);
+
+                //a = a + 1;
+
+                //Console.WriteLine(a);
+
+                Console.WriteLine(++a);
+            }
+        }
+    }
+}
